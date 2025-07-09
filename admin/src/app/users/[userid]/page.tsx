42bef03876f83bf47f6911d959fb6ac159da6182
shaikhsameershabbir <samsk7774@gmail.com>
2025-07-09 15:39:40 +0530

    update

---
+++ admin/src/app/users/[userid]/page.tsx
@@ -0,0 +1,14 @@
+import { AdminLayout } from '@/components/layout/admin-layout'
+import React from 'react'
+
+function page() {
+    return (
+        <AdminLayout>
+            <div>
+                <h1>User Details</h1>
+            </div>
+        </AdminLayout>
+    )
+}
+
+export default page
