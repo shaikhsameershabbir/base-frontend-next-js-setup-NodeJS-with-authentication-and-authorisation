11d0064d3c2e4f672f471d29ca1f564dcd37ee7b
shaikhsameershabbir <samsk7774@gmail.com>
2025-07-25 17:24:04 +0530

    lest go for purmutation combination

--- webSite/src/app/constant/constant.ts
+++ webSite/src/app/constant/constant.ts
@@ -9,7 +9,7 @@ export interface Game {
 }
 
 export interface Message {
-    message: string;
+  message: string;
 }
 
 export interface Bid {
@@ -46,13 +46,13 @@ export interface Fund {
   AvailableBalance: ReactNode;
   boxmessage: ReactNode;
   boxname: ReactNode;
-  addfund:{
+  addfund: {
     boxname: string;
     boxmessage: string;
     AvailableBalance: number;
     infomessage: string;
   }
-  withdrawfund:{  
+  withdrawfund: {
     boxname: string;
     username: string;
     accountnumber: string;
@@ -61,7 +61,7 @@ export interface Fund {
     infomessage: string;
     note: string;
   },
-  addbackdetails:{
+  addbackdetails: {
     accountnumber: string;
     IFSCcode: string;
     Bankname: string;
@@ -79,6 +79,60 @@ export interface GameRate {
   r6: string;
   r7: string;
 }
+
+
+
+// Table data for LeftColumn
+const numbers = [
+  128, 129, 120, 130, 140,
+  137, 138, 139, 149, 159,
+  146, 147, 148, 158, 168,
+  236, 156, 157, 167, 230,
+  245, 237, 238, 239, 249,
+  290, 246, 247, 248, 258,
+  380, 345, 256, 257, 267,
+  470, 390, 346, 347, 348,
+  489, 480, 490, 356, 357,
+  560, 570, 580, 590, 456,
+  579, 589, 670, 680, 690,
+  678, 679, 689, 789, 780,
+  123, 124, 125, 126, 127,
+  150, 160, 134, 135, 136,
+  169, 179, 170, 180, 145,
+  178, 250, 189, 234, 190,
+  240, 269, 260, 270, 235,
+  259, 278, 279, 289, 280,
+  268, 340, 350, 360, 370,
+  349, 359, 369, 379, 389,
+  358, 368, 378, 450, 460,
+  367, 458, 459, 469, 479,
+  457, 467, 468, 478, 569,
+  790, 890, 567, 568, 578
+];
+
+const dpNumbers = [
+  100, 110, 166, 112, 113,
+  119, 200, 229, 220, 122,
+  155, 228, 300, 266, 177,
+  227, 255, 337, 338, 339,
+  335, 336, 355, 400, 366,
+  344, 499, 445, 446, 447,
+  399, 660, 599, 455, 500,
+  588, 688, 779, 699, 799,
+  669, 778, 788, 770, 889,
+  114, 115, 116, 117, 118,
+  277, 133, 224, 144, 226,
+  330, 188, 233, 199, 244,
+  448, 223, 288, 225, 299,
+  466, 377, 440, 388, 334,
+  556, 449, 477, 559, 488,
+  600, 557, 558, 577, 550,
+  880, 566, 800, 667, 668,
+  899, 700, 990, 900, 677
+];
+
+
+
 
 export const games: Game[] = [
   {
@@ -371,7 +425,7 @@ export const passbook: Passbook[] = [
       bidAmount: 50,
       status: "Won"
     }
-  }, 
+  },
 ];
 
 export const funds: Fund[] = [
