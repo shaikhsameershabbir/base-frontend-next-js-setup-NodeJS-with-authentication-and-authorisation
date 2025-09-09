4c85f770c60269b3fd0f2c79048c19ce180ef10f
shaikhsameershabbir <samsk7774@gmail.com>
2025-09-09 10:33:16 +0530

    update

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,5 @@
 
 
-import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './src/config/database';
 import app, { initializeServices } from './src/app';
